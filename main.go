91f979a5314b6bd4c328b9e754d8a1ead7bad8c8
Jason <jason@zgwit.com>
2022-12-20 15:58:01 +0800

    引入mochi-co/mqtt 测试MQTT3.1.1 MQTT5可用

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/mochi-co/mqtt/v2"
+	"github.com/mochi-co/mqtt/v2/hooks/auth"
+	"github.com/mochi-co/mqtt/v2/listeners"
+	"log"
+)
+
+func main() {
+	// Create the new MQTT Server.
+	server := mqtt.New(nil)
+
+	// Allow all connections.
+	_ = server.AddHook(new(auth.AllowHook), nil)
+
+	// Create a TCP listener on a standard port.
+	tcp := listeners.NewTCP("t1", ":1843", nil)
+	err := server.AddListener(tcp)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = server.Serve()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	select {}
+}
