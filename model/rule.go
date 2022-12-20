f175cd17b50eb436c1aeaf3f5d726d45246da5a1
jason <jason@zgwit.com>
2022-12-21 00:33:49 +0800

    定义model

---
+++ model/rule.go
@@ -0,0 +1,21 @@
+package model
+
+import "time"
+
+type Subscribe struct {
+	Id       int64
+	ClientId int64
+
+	Topic    string
+	Disabled bool
+	Created  time.Time `xorm:"created"`
+}
+
+type Publish struct {
+	Id       int64
+	ClientId int64
+
+	Topic    string
+	Disabled bool
+	Created  time.Time `xorm:"created"`
+}
--- model/user.go
+++ model/user.go
@@ -5,5 +5,9 @@ import "time"
 type User struct {
 	Id int64
 
-	Created time.Time `xorm:"created"`
+	Username string
+	Password string
+
+	Disabled bool
+	Created  time.Time `xorm:"created"`
 }
